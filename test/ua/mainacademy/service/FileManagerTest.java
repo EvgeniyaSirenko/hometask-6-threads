bbf674508daa5a3e716e74d1fa8f7f6c9afd9f54
sirenko.jane@gmail.com <Tyag1982>
2021-03-16 15:21:03 +0200

    tests fixed

--- test/ua/mainacademy/service/FileManagerTest.java
+++ test/ua/mainacademy/service/FileManagerTest.java
@@ -14,21 +14,19 @@ class FileManagerTest {
 	private static final String SEPARATOR = System.getProperty("file.separator");
 	private static final String FILES_DIR = MAIN_DIR + SEPARATOR + "files";
 	
-	@BeforeAll
-	public static void addData() {
-		ConnectionInfo connectionTest = new ConnectionInfo(123456, 2L, "123.123.123.123");
-		FileManager.writeConnectionInfoToFile(connectionTest, "connectionsTest.txt", true);
-	}
-	
-	@AfterAll
-	public static void deleteData() {
-		String filePath = FILES_DIR + SEPARATOR + "connectionsTest.txt";
-		File file = new File(filePath);
-		file.delete();
-	}
 	@Test
 	void readConnectionInfoFromFile() {
+		ConnectionInfo connectionTest = new ConnectionInfo(123456, 2L, "123.123.123.123");
+		FileManager.writeConnectionInfoToFile(connectionTest, "connectionsTest.txt", false);
 		List<ConnectionInfo> result = FileManager.readConnectionInfoFromFile("connectionsTest.txt");
 		Assertions.assertEquals("[123456 2 123.123.123.123]", result.toString());
+	}
+	
+	@Test
+	void writeConnectionInfoToFile() {
+		ConnectionInfo connectionTest = new ConnectionInfo(123456, 2L, "123.123.123.123");
+		FileManager.writeConnectionInfoToFile(connectionTest, "connectionsTest.txt", false);
+		Assertions.assertEquals("[123456 2 123.123.123.123]", FileManager.readConnectionInfoFromFile("connectionsTest.txt").toString());
+		
 	}
 }
